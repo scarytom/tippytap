9d340ef4db338db78c3d1bcd3bb91286c33d5ba3
scarytom <t.denley@cantab.net>
2012-01-14 09:51:00 +0000

    some tidying

--- js/script.js
+++ js/script.js
@@ -18,24 +18,49 @@ com.scarytom.TippyTap = function() {
   };
 }
 
-$(document).ready(function() {
-  "use strict";
-  var tippyTap = com.scarytom.TippyTap(),
-      soundChart = new SmoothieChart(),
-      wavSeries = new TimeSeries(),
+com.scarytom.Chart = function(chartElement) {
+  var chartCanvas = $("<canvas id='soundChart' width='400' height='100'></canvas>"),
+      chart = new SmoothieChart(),
       tapSeries = new TimeSeries(),
-      waveData = { "sampleRate": 1, "data": [] },
-      origin = new Date().getTime();
+      wavSeries = new TimeSeries();
 
-  function handleTap() {
+  function tap() {
     var tapTime = new Date().getTime();
-    tippyTap.tap();
     tapSeries.append(tapTime - 10, 0);
     tapSeries.append(tapTime, 10000);
     tapSeries.append(tapTime + 10, 0);
-    var note = $("<span><span/>");
-    note.text(tippyTap.lastTime());
-    $("#main").append(note);
+  }
+
+  function plot(time, value) {
+    wavSeries.append(time, value);
+  }
+
+  $(chartElement).append(chartCanvas);
+  chart.addTimeSeries(wavSeries, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+  chart.addTimeSeries(tapSeries, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 4 });
+  chart.streamTo(chartCanvas.get(0), 50);
+  return {
+    "tap": tap,
+    "plot": plot
+  };
+}
+
+$(document).ready(function() {
+  "use strict";
+  var tippyTap = com.scarytom.TippyTap(),
+      chart = com.scarytom.Chart($("#graphsection")),
+      waveData = { "sampleRate": 1, "data": [] },
+      origin = new Date().getTime();
+
+  function log(message) {
+    $("#log").append($("<span></span>").text(message));
+    $("#log").append($("<br/>"));
+  }
+
+  function handleTap() {
+    tippyTap.tap();
+    chart.tap();
+    log(tippyTap.lastTime());
   }
 
   function fileSelected(evt) {
@@ -56,15 +81,10 @@ $(document).ready(function() {
         sampleNo = Math.ceil(waveData.sampleRate * offset / 1000),
         sample = waveData.data[sampleNo];
     sample = sample ? sample : 0;
-    wavSeries.append(new Date().getTime(), sample);
+    chart.plot(new Date().getTime(), sample);
   }
 
   setInterval(plot, 30);
-      
-  soundChart.addTimeSeries(wavSeries, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
-  soundChart.addTimeSeries(tapSeries, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 4 });
-  soundChart.streamTo($("#soundChart").get(0), 50);
-
   $(document).keypress(handleTap);
   $("#files").change(fileSelected);
 });
